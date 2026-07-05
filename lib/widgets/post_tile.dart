a21d920ea079e5e713073a0ba2232a6cfa45cc83
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2026-07-05 21:04:08 +0800

    [Feature] Add PostTile widget (#1)

---
+++ lib/widgets/post_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:dart_flutter/models/models.dart' show PostModel;
+
+class PostTile extends StatelessWidget {
+  final PostModel post;
+
+  const PostTile({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(title: Text(post.title), subtitle: Text(post.body));
+  }
+}
