e0c77321734bdedf67012adee481b9b11cc75622
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2026-07-06 19:40:04 +0800

    Add PostList widget (#2)

---
+++ lib/widgets/post_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:dart_flutter/models/models.dart' show PostModel;
+import 'package:dart_flutter/widgets/widgets.dart' show PostTile;
+
+class PostList extends StatelessWidget {
+  final List<PostModel> posts;
+
+  const PostList({super.key, required this.posts});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: posts.length,
+      itemBuilder: (_, int index) => PostTile(post: posts[index]),
+      separatorBuilder: (_, __) => Divider(height: 1),
+    );
+  }
+}
