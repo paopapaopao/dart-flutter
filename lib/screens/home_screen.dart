ff18197b239276ad25efed8d1a47ce79e1291302
paopapaopao <paolo.casugay@gmail.com>
2026-07-06 20:21:13 +0800

    Refactor

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,45 +1,39 @@
 import 'package:flutter/material.dart';
 
 import 'package:dart_flutter/models/models.dart' show PostModel;
+import 'package:dart_flutter/services/services.dart' show ApiService;
 import 'package:dart_flutter/widgets/widgets.dart' show PostList;
 
 class HomeScreen extends StatelessWidget {
-  final List<PostModel> _posts = [
-    PostModel(
-      title: 'Post 1',
-      body:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra elementum lorem vel commodo. Aliquam erat volutpat. Phasellus lobortis ullamcorper pulvinar.',
-    ),
-    PostModel(
-      title: 'Post 2',
-      body:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra elementum lorem vel commodo. Aliquam erat volutpat. Phasellus lobortis ullamcorper pulvinar.',
-    ),
-    PostModel(
-      title: 'Post 3',
-      body:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra elementum lorem vel commodo. Aliquam erat volutpat. Phasellus lobortis ullamcorper pulvinar.',
-    ),
-    PostModel(
-      title: 'Post 4',
-      body:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra elementum lorem vel commodo. Aliquam erat volutpat. Phasellus lobortis ullamcorper pulvinar.',
-    ),
-  ];
-
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ApiService api = ApiService();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
-        title: Text('Home'),
         foregroundColor: Colors.white,
+        title: Text('Home'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(4),
-        child: PostList(posts: _posts),
+      body: FutureBuilder<List<PostModel>>(
+        future: api.readPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No posts found'));
+          }
+
+          return PostList(posts: snapshot.data!);
+        },
       ),
     );
   }
