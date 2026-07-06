6989c68ffffdaf94fe4a99fa6168886bdea9dc9d
paopapaopao <paolo.casugay@gmail.com>
2026-07-06 20:15:57 +0800

    Change PostModel: Add id, fromJson

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,6 +1,11 @@
 class PostModel {
+  final int id;
   final String title;
   final String body;
 
-  const PostModel({required this.title, required this.body});
+  PostModel({required this.id, required this.title, required this.body});
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(id: json['id'], title: json['title'], body: json['body']);
+  }
 }
