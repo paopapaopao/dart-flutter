925d2c56e2d4ce1f9f65a1cd9bf2e8bbd8fb4b7b
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2026-07-09 19:48:33 +0800

    [Feature] Add scroll to top (#5)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,13 +4,58 @@ import 'package:dart_flutter/models/models.dart';
 import 'package:dart_flutter/services/services.dart';
 import 'package:dart_flutter/widgets/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final ApiService api = ApiService();
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiService _api = ApiService();
+  final ScrollController _scrollController = ScrollController();
+
+  late final Future<List<PostModel>> _postsFuture;
+
+  bool _isShown = false;
+
+  void _listener() {
+    const threshold = 300.0;
+    final shouldShow = _scrollController.offset > threshold;
+
+    if (shouldShow != _isShown) {
+      setState(() {
+        _isShown = shouldShow;
+      });
+    }
+  }
+
+  void _handlePress() {
+    _scrollController.animateTo(
+      0,
+      duration: Duration(milliseconds: 400),
+      curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _postsFuture = _api.readPosts();
+    _scrollController.addListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_listener);
+    _scrollController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(_) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
@@ -18,22 +63,40 @@ class HomeScreen extends StatelessWidget {
         title: Text('Home'),
       ),
       body: FutureBuilder<List<PostModel>>(
-        future: api.readPosts(),
-        builder: (context, snapshot) {
+        future: _postsFuture,
+        builder: (_, AsyncSnapshot<List<PostModel>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+            return Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            );
           }
 
           if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
+            return Container(
+              alignment: Alignment.center,
+              child: Text(snapshot.error.toString()),
+            );
           }
 
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No posts found'));
+          final posts = snapshot.data;
+
+          if (posts == null || posts.isEmpty) {
+            return Center(child: Text('Posts not found'));
           }
 
-          return PostList(posts: snapshot.data!);
+          return PostList(posts: posts, controller: _scrollController);
         },
+      ),
+      floatingActionButton: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: _isShown
+            ? FloatingActionButton(
+                key: ValueKey('scrollToTop'),
+                onPressed: _handlePress,
+                child: Icon(Icons.arrow_upward),
+              )
+            : SizedBox.shrink(),
       ),
     );
   }
--- lib/widgets/post_list.dart
+++ lib/widgets/post_list.dart
@@ -5,12 +5,14 @@ import 'package:dart_flutter/widgets/widgets.dart';
 
 class PostList extends StatelessWidget {
   final List<PostModel> posts;
+  final ScrollController? controller;
 
-  const PostList({super.key, required this.posts});
+  const PostList({super.key, required this.posts, this.controller});
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
+      controller: controller,
       itemCount: posts.length,
       itemBuilder: (_, int index) => PostTile(post: posts[index]),
       separatorBuilder: (_, __) => Divider(height: 1),
