adb696c0328f48dd8374d637feba164a0194249c
paopapaopao <paolo.casugay@gmail.com>
2026-07-18 21:34:02 +0800

    Change: Edit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,17 +6,32 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:dart_flutter/app.dart';
+import 'package:dart_flutter/providers/providers.dart';
+import 'package:dart_flutter/repositories/repositories.dart';
+import 'package:dart_flutter/services/services.dart';
 
 void main() {
-  testWidgets('should see the text "Dart Flutter"', (
-    WidgetTester tester,
-  ) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+  testWidgets('AppBar displays the title "Home"', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<PostProvider>(
+            create: (_) => PostProvider(PostRepository(ApiService())),
+          ),
+        ],
+        child: const App(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Dart Flutter'), findsOneWidget);
+    expect(
+      find.byWidgetPredicate(
+        (w) => w is AppBar && (w.title as Text).data == 'Home',
+      ),
+      findsOneWidget,
+    );
   });
 }
